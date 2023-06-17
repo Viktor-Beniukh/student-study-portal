34bead4e3f36d55cd2bf39b2883a68dd65b13246
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-06-17 21:37:06 +0300

    Added models for students app

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from students.models import Note, Homework, Todo
+
+admin.site.register(Note)
+admin.site.register(Homework)
+admin.site.register(Todo)
